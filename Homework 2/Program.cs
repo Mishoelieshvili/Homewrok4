2c79124686557382d364f38e19e8f5f9d98eb984
mishoelieshvili <mishoelieshvili@gmail.com>
2023-02-27 15:27:22 +0400

    homework

---
+++ Homework 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace Homeworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Press Enter");
+            var someword = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(someword + "misho elieshvili");
+
+        }
+    }
+}
